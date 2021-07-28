696ca8eface0e6bd13a733694033be8c641532db
ALIAKSEI SERBAT <aliaksei.serbat@paxful.com>
2021-07-28 21:45:12 +0300

    Fixed printColor() method

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -28,7 +28,7 @@ public class HomeWorkApp {
 
         if (value <= 0) {
             System.out.println("Красный");
-        } else if (value > 0 && 0 <= 100) {
+        } else if (value > 0 && value <= 100) {
             System.out.println("Желтый");
         } else if (value > 100) {
             System.out.println("Зеленый");
