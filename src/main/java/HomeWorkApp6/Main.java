c0563434465dc32a5d801809d28c050c9aeb5b6e
AlexeySerbat <alexeyserbat2517@gmail.com>
2021-08-14 19:16:30 +0300

    Lesson 6

---
+++ src/main/java/HomeWorkApp6/Main.java
@@ -0,0 +1,42 @@
+package HomeWorkApp6;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[][] arr = new String[][]{{"3", "12", "14", "22"}, {"33", "12", "44", "1"}, {"1", "2", "15", "2"}, {"55", "22", "21", "32"}};
+        try {
+            try {
+                int result = countArray(arr);
+                System.out.println(result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Размер массива превышен!");
+            }
+        }
+        catch (MyArrayDataException e) {
+            System.out.println("Неправильное значение массива!");
+            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+        }
+    }
+
+    public static int countArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException("Размер массива должен содержать 4 элемента");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException("Размер массива должен быть 4х4");
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/HomeWorkApp6/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package HomeWorkApp6;
+
+public class MyArraySizeException extends Exception {
+    MyArraySizeException(String msg) {
+        super(msg);
+    }
+}
